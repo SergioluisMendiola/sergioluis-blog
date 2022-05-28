bb3a8875ce7b4dc88f941b0ea969000afb674f43
Sergioluis <mendiolasergioluis@gmail.com>
2022-05-28 10:36:19 -0400

    SQLite to PostgresSQL

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from functools import wraps
 from flask_gravatar import Gravatar
 import locale
+import os
 
 locale.setlocale(locale.LC_TIME, '')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('appSecretKey')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -52,7 +53,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
